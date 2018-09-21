843372f629647aca8bb8287819895143a9c0b2d9
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2018-09-21 17:44:48 +0300

    Adds App specific configs

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+export default {
+  swipe: {
+    swipeDuration: 500,
+    swipeToOpenPercent: 40
+  },
+  navigation: {
+    'activeTodos': {
+      title: 'My Todo List'
+    },
+    'completedTodos': {
+      title: 'Completed Todo List'
+    }
+  }
+}
